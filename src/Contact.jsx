0911848f2bc6028dfe4165605d56a7d06dc4fe5c
Saimum-14028 <md.amiralfahim@gmail.com>
2024-02-17 23:58:37 +0600

    project  page completed alhamdulillah

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -36,25 +36,24 @@ const Contact = () => {
     return (
         <div className="py-8 px-4 mx-auto max-w-2xl lg:py-16">
             <form onSubmit={handleMessage} action="#">
-                        <div className="grid gap-4 sm:grid-cols-2 sm:gap-6">
-                            <div className="w-full">
-                                <label htmlFor="name" className="block mb-2 text-sm font-medium ">Name</label>
-                                <input type="text" name="name" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Your Name" required></input>
-                            </div>
-                            <div className="w-full">
-                                <label htmlFor="email" className="block mb-2 text-sm font-medium ">Email</label>
-                                <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Your Email" required></input>
-                            </div>
-                            <div className="sm:col-span-2">
-                                <label htmlFor="message" className="block mb-2 text-sm font-medium ">Message</label>
-                                <textarea id="message" rows="8" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Your Message" required></textarea>
-                            </div>
-                            
-                        </div>
-                        <div className="form-control mt-6 p-0">
-                            <button type='submit' className="btn bg-green-500 text-white">Send Message</button>
-                        </div>
-                    </form>
+                <div className="grid gap-4 sm:grid-cols-2 sm:gap-6">
+                    <div className="w-full">
+                        <label htmlFor="name" className="block mb-2 text-sm font-medium ">Name</label>
+                        <input type="text" name="name" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Your Name" required></input>
+                    </div>
+                    <div className="w-full">
+                        <label htmlFor="email" className="block mb-2 text-sm font-medium ">Email</label>
+                        <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Your Email" required></input>
+                    </div>
+                    <div className="sm:col-span-2">
+                        <label htmlFor="message" className="block mb-2 text-sm font-medium ">Message</label>
+                        <textarea id="message" rows="8" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Your Message" required></textarea>
+                    </div>
+                </div>
+                <div className="form-control mt-6 p-0">
+                    <button type='submit' className="btn bg-green-500 text-white">Send Message</button>
+                </div>
+            </form>
         </div>
     );
 };
--- src/Projects.jsx
+++ src/Projects.jsx
@@ -2,8 +2,300 @@ import React from 'react';
 
 const Projects = () => {
     return (
-        <div>
-            
+        <div className='flex justify-center gap-5'>
+            <div className="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+  
+                <div className="flex items-baseline text-gray-900 dark:text-white">
+                    <span className="text-3xl font-semibold">Brand Shop</span>
+                </div>
+                <h5 className="mb-4 text-lg font-medium text-gray-500 dark:text-gray-400">
+                    HTML, CSS, Tailwind, React
+                </h5>
+                <ul role="list" className="space-y-5 my-7">
+                    <li className="flex items-center">
+                    <svg
+                        className="flex-shrink-0 w-4 h-4 text-green-700 dark:text-green-500"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                    >
+                        <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                    </svg>
+                    <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">
+                    Popular Brand in one place
+                    </span>
+                    </li>
+                    <li className="flex">
+                    <svg
+                        className="flex-shrink-0 w-4 h-4 text-green-700 dark:text-green-500"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                    >
+                        <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                    </svg>
+                    <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">
+                    Easily add and update product
+                    </span>
+                    </li>
+                    <li className="flex">
+                    <svg
+                        className="flex-shrink-0 w-4 h-4 text-green-700 dark:text-green-500"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                    >
+                        <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                    </svg>
+                    <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">
+                    Create Account via GMAIL. No extra effort needed
+                    </span>
+                    </li>
+                    <li className="flex">
+                    <svg
+                        className="flex-shrink-0 w-4 h-4 text-green-700 dark:text-green-500"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                    >
+                        <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                    </svg>
+                    <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">
+                    Theme preferences for user
+                    </span>
+                    </li>
+                    <li className="flex">
+                    <svg
+                        className="flex-shrink-0 w-4 h-4 text-green-700 dark:text-green-500"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                    >
+                        <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                    </svg>
+                    <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">
+                    User friendly design
+                    </span>
+                    </li>
+                    
+                </ul>
+                <div className='flex justify-around gap-2'>
+                    <button
+                        type="button"
+                        className="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-blue-200 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-blue-900 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex justify-center w-full text-center"
+                    >
+                        <a href="https://my-brand-shop-client-side.web.app/" target="_blank">Live Website</a>
+                    </button>
+                    <button
+                        type="button"
+                        className="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-blue-200 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-blue-900 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex justify-center w-full text-center"
+                    >
+                        <a href="https://github.com/Saimum-14028/brand-shop-10" target="_blank">Github Repo</a>
+                        
+                    </button>
+                </div>
+            </div>
+
+            <div className="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+  
+                <div className="flex items-baseline text-gray-900 dark:text-white">
+                    <span className="text-3xl font-semibold">Bite</span>
+                </div>
+                <h5 className="mb-4 text-lg font-medium text-gray-500 dark:text-gray-400">
+                    HTML, CSS, Tailwind, React
+                </h5>
+                <ul role="list" className="space-y-5 my-7">
+                    <li className="flex items-center">
+                    <svg
+                        className="flex-shrink-0 w-4 h-4 text-green-700 dark:text-green-500"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                    >
+                        <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                    </svg>
+                    <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">
+                    All types of food in one place
+                    </span>
+                    </li>
+                    <li className="flex">
+                    <svg
+                        className="flex-shrink-0 w-4 h-4 text-green-700 dark:text-green-500"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                    >
+                        <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                    </svg>
+                    <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">
+                    User can add and update food items
+                    </span>
+                    </li>
+                    <li className="flex">
+                    <svg
+                        className="flex-shrink-0 w-4 h-4 text-green-700 dark:text-green-500"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                    >
+                        <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                    </svg>
+                    <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">
+                    Create Account via GMAIL. No extra effort needed
+                    </span>
+                    </li>
+                    <li className="flex">
+                    <svg
+                        className="flex-shrink-0 w-4 h-4 text-green-700 dark:text-green-500"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                    >
+                        <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                    </svg>
+                    <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">
+                    Theme preferences for user
+                    </span>
+                    </li>
+                    <li className="flex">
+                    <svg
+                        className="flex-shrink-0 w-4 h-4 text-green-700 dark:text-green-500"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                    >
+                        <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                    </svg>
+                    <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">
+                    Privacy protected
+                    </span>
+                    </li>
+                    
+                </ul>
+                <div className='flex justify-around gap-2'>
+                    <button
+                        type="button"
+                        className="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-blue-200 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-blue-900 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex justify-center w-full text-center"
+                    >
+                        <a href="https://bite-client-side.web.app/" target="_blank">Live Website</a>
+                    </button>
+                    <button
+                        type="button"
+                        className="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-blue-200 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-blue-900 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex justify-center w-full text-center"
+                    >
+                        <a href="https://github.com/Saimum-14028/bite" target="_blank">Github Repo</a>
+                        
+                    </button>
+                </div>
+            </div>
+
+            <div className="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+  
+                <div className="flex items-baseline text-gray-900 dark:text-white">
+                    <span className="text-3xl font-semibold">Fast</span>
+                </div>
+                <h5 className="mb-4 text-lg font-medium text-gray-500 dark:text-gray-400">
+                    HTML, CSS, Tailwind, React
+                </h5>
+                <ul role="list" className="space-y-5 my-7">
+                    <li className="flex items-center">
+                    <svg
+                        className="flex-shrink-0 w-4 h-4 text-green-700 dark:text-green-500"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                    >
+                        <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                    </svg>
+                    <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">
+                    All types of parcel booking
+                    </span>
+                    </li>
+                    <li className="flex">
+                    <svg
+                        className="flex-shrink-0 w-4 h-4 text-green-700 dark:text-green-500"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                    >
+                        <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                    </svg>
+                    <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">
+                    User can book parcel
+                    </span>
+                    </li>
+                    <li className="flex">
+                    <svg
+                        className="flex-shrink-0 w-4 h-4 text-green-700 dark:text-green-500"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                    >
+                        <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                    </svg>
+                    <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">
+                    Create Account via GMAIL. No extra effort needed
+                    </span>
+                    </li>
+                    <li className="flex">
+                    <svg
+                        className="flex-shrink-0 w-4 h-4 text-green-700 dark:text-green-500"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                    >
+                        <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                    </svg>
+                    <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">
+                    Theme preferences for user
+                    </span>
+                    </li>
+                    <li className="flex">
+                    <svg
+                        className="flex-shrink-0 w-4 h-4 text-green-700 dark:text-green-500"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                    >
+                        <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                    </svg>
+                    <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">
+                    User friendly design
+                    </span>
+                    </li>
+                    
+                </ul>
+                <div className='flex justify-around gap-2'>
+                    <button
+                        type="button"
+                        className="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-blue-200 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-blue-900 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex justify-center w-full text-center"
+                    >
+                        <a href="https://fast-client-side.web.app" target="_blank">Live Website</a>
+                    </button>
+                    <button
+                        type="button"
+                        className="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-blue-200 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-blue-900 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex justify-center w-full text-center"
+                    >
+                        <a href="https://github.com/Saimum-14028/fast" target="_blank">Github Repo</a>
+                        
+                    </button>
+                </div>
+            </div>
         </div>
     );
 };
